b3989092188659aaae04efe3ae4f56aae04f6a19
AJ <aj@example.com>
2022-03-05 23:36:26 -0500

    Second passing test

--- src/primeFactors.js
+++ src/primeFactors.js
@@ -1,5 +1,12 @@
-function primeFactors(){
-    return [];
+function primeFactors(suppliedNumber){
+    let number = suppliedNumber;
+    let primes = [];
+
+    if(number > 1){
+        primes.push(number);
+    }
+    
+    return primes;
 }
 
 module.exports = primeFactors;
--- tests/primefactors.test.js
+++ tests/primefactors.test.js
@@ -3,3 +3,7 @@ const primeFactors = require('../src/primeFactors');
 test('prime factor of 1 is none', ()=>{
     expect(primeFactors(1)).toStrictEqual([]);
 })
+
+test('prime factors of 2 is 2', ()=> {
+    expect(primeFactors(2)).toStrictEqual([2]);
+})
