82f3da7f92e66c428c58d09259a78682d52d9fef
AJ <aj@example.com>
2022-03-05 23:37:10 -0500

    Third passing test

--- tests/primefactors.test.js
+++ tests/primefactors.test.js
@@ -7,3 +7,7 @@ test('prime factor of 1 is none', ()=>{
 test('prime factors of 2 is 2', ()=> {
     expect(primeFactors(2)).toStrictEqual([2]);
 })
+
+test('prime factors of 3 is 3', ()=> {
+    expect(primeFactors(3)).toStrictEqual([3]);
+})
