be8eb11071be9ed684e9355ed01ab663cf99057a
AJ <aj@example.com>
2022-03-05 23:47:05 -0500

    Final refactor

--- src/primeFactors.js
+++ src/primeFactors.js
@@ -3,11 +3,10 @@ function primeFactors(suppliedNumber){
     let candidate = 2;
     let primes = [];
 
-    while(number > 1){
+    for(candidate; number > 1; candidate += 1){
         for(number; number % candidate === 0; number /= candidate){
             primes.push(candidate);
         }
-        candidate += 1;
     }
 
     return primes;
