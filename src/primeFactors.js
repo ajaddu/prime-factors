73a151a55753cf0b52aa66a2fd8e9a3f34e298a1
AJ <aj@example.com>
2022-03-05 23:44:48 -0500

    Seventh passing test

--- src/primeFactors.js
+++ src/primeFactors.js
@@ -1,18 +1,19 @@
 function primeFactors(suppliedNumber){
     let number = suppliedNumber;
+    let candidate = 2;
     let primes = [];
 
-    if(number > 1){
-        while(number % 2 === 0){
-            primes.push(2);
-            number /= 2;
+    while(number > 1){
+        while(number % candidate === 0){
+            primes.push(candidate);
+            number /= candidate;
         }
-
-        if(number > 1){
-            primes.push(number);
-        }
+        candidate += 1;
     }
     
+    if(number > 1){
+        primes.push(number);
+    }
     return primes;
 }
 
